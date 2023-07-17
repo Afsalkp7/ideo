6ab07f99ad7185916b70e062516f1d12ae70cf21
Afsalkp7 <afsalkpmanu31@gmail.com>
2023-07-17 12:27:23 +0530

    index added

---
+++ sbt.js
@@ -0,0 +1,12 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }  
+  function openJumb() {
+    document.getElementById("jumbBox").style.height = "100vh";
+  }
+  function closeJumb() {
+    document.getElementById("jumbBox").style.height = "0vh";
+  } 
